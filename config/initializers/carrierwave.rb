389984a1d18b5a84e0106601ddde88dad3b46958
Huy Nguyen <nqhuy1811@gmail.com>
2018-05-15 23:14:22 +0700

    xxx

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
     config.fog_credentials = {
-      provider:              'AWS', # required
-      aws_access_key_id:     Rails.application.secrets.aws_access_key_id, # required
-      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key, # required
+      # provider:              'AWS', # required
+      # aws_access_key_id:     Rails.application.secrets.aws_access_key_id, # required
+      # aws_secret_access_key: Rails.application.secrets.aws_secret_access_key, # required
       region:                'us-east-1', # optional, defaults to 'us-east-1'
     }
     config.fog_directory  = 'doge-much-buddies' # required
