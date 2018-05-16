623acf348ba7a74bd9b06bbda96be84c481e7b89
hai tran <haitranviet96@gmail.com>
2018-05-16 11:10:54 +0700

    complete social login

--- app/controllers/users/omniauth_controller.rb
+++ app/controllers/users/omniauth_controller.rb
@@ -1,17 +1,5 @@
 class Users::OmniauthController < Devise::OmniauthCallbacksController
 
-  # facebook callback
-  def facebook
-    @user = User.create_from_provider_data(request.env['omniauth.auth'])
-    if @user.persisted?
-      sign_in_and_redirect @user
-      set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
-    else
-      flash[:error] = 'There was a problem signing you in through Facebook. Please register or try signing in later.'
-      redirect_to new_user_registration_url
-    end
-  end
-
   # github callback
   def github
     @user = User.create_from_provider_data(request.env['omniauth.auth'])
@@ -39,13 +27,10 @@ class Users::OmniauthController < Devise::OmniauthCallbacksController
   # twitter callback
   def twitter
     @user = User.create_from_provider_data(request.env['omniauth.auth'])
-    byebug
     if @user.persisted?
-      byebug
       sign_in_and_redirect @user
       set_flash_message(:notice, :success, kind: 'Twitter') if is_navigational_format?
     else
-      byebug
       redirect_to(new_user_registration_url, {:flash => { :error => 'There was a problem signing you in through Twitter. Please register or try signing in later.' }})
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :validatable,
          :recoverable, :rememberable, :trackable, :timeoutable,
-         :omniauthable, omniauth_providers: [:facebook, :github, :google_oauth2, :twitter]
+         :omniauthable, omniauth_providers: [:github, :google_oauth2, :twitter]
 
   acts_as_voter
   acts_as_follower
@@ -25,11 +25,23 @@ class User < ApplicationRecord
   friendly_id :name, use: [:slugged, :finders]
 
   def self.create_from_provider_data(provider_data)
-    where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do | user |
-      user.name = provider_data.info.name
-      user.email = provider_data.info.email
-      user.encrypted_password = Devise.friendly_token[0, 20]
-      user.save!
+    if where(email: provider_data.info.email).first.nil?
+      where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
+        user.name = provider_data.info.name
+        user.email = provider_data.info.email
+        user.location = provider_data.info.location
+        user.about = provider_data.extra.raw_info.description
+        if provider_data.info.image.present?
+          user.remote_avatar_url = provider_data.info.image
+        end
+        if provider_data.extra.raw_info.profile_banner_url.present?
+          user.remote_cover_url = provider_data.extra.raw_info.profile_banner_url
+        end
+        user.password = Devise.friendly_token[0, 20]
+        user.save!
+      end
+    else
+      where(email: provider_data.info.email).first
     end
   end
 end
